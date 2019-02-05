3f888ee915c0e2ac5ae34e58fcc15aa289956bb0
oghusky <philiposimmons@gmail.com>
2019-02-04 23:51:11 -0500

    changed navbar scroll function

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,26 +1,20 @@
 $(document).ready(function() {
   //   controls progress bar animation
   let barsPos = $("#progress-header").offset().top;
-  $(window).on("scroll", function() {
+  window.addEventListener("scroll", function() {
     if (barsPos - 400 < $(window).scrollTop()) {
       $("#progress-div").addClass("progress-animate");
       $(window).off("scroll");
     }
     //   controls nav position
-    const navBar = $("nav");
-    const canvas = $("canvas");
-    if (
-      $(window).scrollTop() + canvas.height() >=
-      $("#about-sect").position().top + 580
-    ) {
-      navBar.addClass("fixed-top");
+    const navBar = document.querySelector("nav");
+    const canvas = document.querySelector("canvas");
+    const aboutSect = document.querySelector("#about-sect");
+    if (window.pageYOffset + canvas.offsetHeight >= aboutSect.offsetTop + 500) {
+      navBar.classList.add("fixed-top");
     }
-    if (
-      $(window).scrollTop() + canvas.height() <=
-        $("#about-sect").position().top + 580 ||
-      $(window).scrollTop() === 0
-    ) {
-      navBar.removeClass("fixed-top");
+    if (window.pageYOffset + canvas.offsetHeight <= aboutSect.offsetTop + 500) {
+      navBar.classList.remove("fixed-top");
     }
   });
 });
